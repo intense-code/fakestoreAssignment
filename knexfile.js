ecb5c0abd2079011f04cde7eca7d9815a2d81f1c
Rivit Not Vivit <jack.problems@protonmail.com>
2025-11-10 14:52:05 -0800

    Added ssl

--- knexfile.js
+++ knexfile.js
@@ -63,13 +63,16 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false  // Important for Supabase
+    },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
     tableName: 'knex_migrations',
-    schemaName: 'production',  // <--- new schema
+    schemaName: 'public',  // <--- new schema
     directory: './migrations',
   },
         seeds: {
